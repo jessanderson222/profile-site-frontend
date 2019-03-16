ff7e893dc4dc34702204ba3eb273510fe717d0d8
jessanderson222 <jessanderson222@gmail.com>
2019-03-16 17:17:15 -0400

    Added bio, work, and social containers

---
+++ src/components/BioContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class BioContainer extends Component {
+  render() {
+    return <h2>BioContainer</h2>;
+  }
+}
+
+export default BioContainer;
--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import BioContainer from "./BioContainer";
+import WorkContainer from "./WorkContainer";
+import SocialContainer from "./SocialContainer";
 
 class ContentContainer extends Component {
   render() {
     return (
-      <div>
-        <h4>:)</h4>
+      <div id="content-div">
+        <div className="container">
+          <BioContainer />
+        </div>
+        <div className="container">
+          <WorkContainer />
+        </div>
+        <div className="container">
+          <SocialContainer />
+        </div>
       </div>
     );
   }

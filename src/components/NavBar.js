201fa3d149daf1e2b1c39922e6e244bb8e023bbb
jessanderson222 <jessanderson222@gmail.com>
2019-03-20 10:25:43 -0400

    Semantic installed, icon buttons in nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { Button, Icon } from "semantic-ui-react";
 
 class SocialContainer extends Component {
   render() {
     return (
       <div>
-        <button className="social">Twitter</button>
-        <button className="social">LinkedIn</button>
-        <button className="social">GitHub</button>
-        <button className="social">Email</button>
+        <Button icon>
+          <Icon name="twitter" />
+        </Button>
+        <Button icon>
+          <Icon name="linkedin" />
+        </Button>
+        <Button icon>
+          <Icon name="github" />
+        </Button>
+        <Button icon>
+          <Icon name="envelope" />
+        </Button>
         <h4 className="nav-text">Work</h4>
       </div>
     );

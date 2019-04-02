0fc70bfc554d646397ce0ffed3dca7e6561ab9c2
jessanderson222 <jessanderson222@gmail.com>
2019-04-02 11:00:52 -0400

    Text no longer on top of itself

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -4,10 +4,11 @@ class Hello extends Component {
   render() {
     return (
       <div className="hello">
-        <h1 className="text">Hello.</h1>
-        <br />
-
-        <h1 className="text">It's Jessie Anderson.</h1>
+        <p className="text">
+          Hello.
+          <br />
+          It's Jessie Anderson
+        </p>
       </div>
     );
   }

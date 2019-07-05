9b3bfee5dd08dd350567415c375846050e88d4b5
jessanderson222 <jessanderson222@gmail.com>
2019-07-05 18:42:41 -0400

    Routes for resume work

---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Document } from "react-pdf";
+
+class Resume extends Component {
+  render() {
+    return (
+      <div>
+        <h3>:)</h3>
+      </div>
+    );
+  }
+}
+export default Resume;

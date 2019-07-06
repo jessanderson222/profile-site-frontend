eb7b6e47a68322c569b5e4e57ebc43c5f11d0d3d
jessanderson222 <jessanderson222@gmail.com>
2019-07-06 15:46:42 -0400

    Resume pdf upload

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
-import { Document } from "react-pdf";
+import resume from "./resume.pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${
+  pdfjs.version
+}/pdf.worker.js`;
 
 class Resume extends Component {
+  state = {
+    numPages: 1,
+    pageNumber: 1
+  };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
   render() {
+    const { pageNumber, numPages } = this.state;
+
     return (
       <div>
-        <h3>:)</h3>
+        <div className="resume">
+          <Document file={resume} onLoadSuccess={this.onDocumentLoadSuccess}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+        </div>
       </div>
     );
   }
